a8d1338ebd90b6b18ce9068a50f5111cadb07bd1
Mukund Lakshman <yaymukund@gmail.com>
2014-02-21 15:04:50 -0500

    Using prototypal inheritance.

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var io = require('socket.io').listen(3001),
     Client = require('./lib/client');
 
 io.sockets.on('connection', function(socket) {
-  var client = new Client(socket);
+  var client = Client.create(socket);
   socket.on('get_room', client.getRoom.bind(client));
 });
